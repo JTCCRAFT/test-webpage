f41cdad970a7bf0c55fae10b27336b7fc8283bad
JTCCRAFT <jctcraft@gmail.com>
2026-05-27 11:57:54 -0500

    Add imbored.bat for fun output and pauses
    Create a batch file that displays messages and waits.

---
+++ imbored.bat
@@ -0,0 +1,11 @@
+echo NOTE: this was just for fun :D
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+timeout /t 5 /nobreak
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+echo a
+echo b
+timeout /t 5 /nobreak
+dir
+echo b
+timeout /t 5 /nobreak
+cls
